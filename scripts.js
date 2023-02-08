a44fe590042e9574de741ba74264e0a560435c3c
CaioMantovaniBorba <https://github.com/CaioMantovaniBorba>
2023-02-08 13:38:51 -0300

    Update function call

--- scripts.js
+++ scripts.js
@@ -1,8 +1,5 @@
 const generateColor = () => {
-  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   document.body.style.backgroundColor = "#" + randomColor;
   color.innerHTML = "#" + randomColor;
 }
-
-generate.addEventListener("click", generateColor);
-generateColor();
